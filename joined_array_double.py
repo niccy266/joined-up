c57c760b7e1ddc5edf6186c93eebdc2c454a5c97
niccy266 <niccy266@gmail.com>
2022-01-28 15:56:07 +1300

    nothing works

--- joined_array_double.py
+++ joined_array_double.py
@@ -46,6 +46,8 @@ def main():
         sys.exit(1)
 
     dictionary = []
+    nodes = []
+
 
     #read in dictionary
     for line in sys.stdin :
@@ -67,7 +69,14 @@ def main():
     #remove duplicates
     dictionary = list(dict.fromkeys(dictionary))
 
-    halves = calc_halves(dictionary)
+    #use dictionary to create a node for each word
+    for i, word in enumerate(dictionary) :
+        nodes.append(Node(i, word))
+
+    halves = []
+    for node in nodes :
+        halves.append(node.half)
+    halves_a = np.array(halves, dtype=int)
 
     print(str_start, str_end)
     #search for links in singled and double joined mode and print results
@@ -86,36 +95,21 @@ def main():
     sys.exit(0)
 
 
-def calc_halves(d):
-    halves = []
-    for w in d :
-        halves.append(int((len(w) + 1) / 2))
-    return np.array(halves, dtype=int)
-
-
-def end_has_no_links(d, end_i, link_type):
-    end = d[end_i]
-    end_len = len(end)
-    suffix_len = int((end_len + 1) / 2)
-
-    if link_type == SINGLE:
-        match_calc = single_calc
-        min_len = 0
-    else:
-        match_calc = double_calc
-        min_len = suffix_len
+def end_has_no_links(d, end, match_calc, min_len):
+    
+    #reverse end
+    end = end[::-1]
+    suffix_len = int((len(end) + 1) / 2)
 
     #reverse all words
-    end = end[::-1]
     reverse_dict = [w[::-1] for w in d]
     reverse_dict.sort()
 
-    for match_len in range(min_len, end_len + 1):
-        print("finding all words starting from", end[-match_len:][::-1], "to", (end[-match_len:-1] + chr(ord(end[-1])+1))[::-1])
-        left = bisect(reverse_dict, end[-match_len:]) - 1
-        right = bisect(reverse_dict, end[-match_len:] + chr(ord(end[-1])+1)) + 1
-        print(left, right)
-
+    for match_len in range(min_len, len(end) + 1):
+        
+        left = bisect(reverse_dict, end[-match_len:])
+        right = bisect(reverse_dict, end[-match_len:-1] + chr(ord(end[-1])+1))
+        
         for r, rw in enumerate(reverse_dict[left:right]):
 
             r += left
@@ -123,26 +117,25 @@ def end_has_no_links(d, end_i, link_type):
             #check if the suffix we are checking is long enough to join for either word
             prefix_len = int((len(rw) + 1) / 2)
             match = match_calc(suffix_len, prefix_len)
-            
             if (match_len < match):
-                #continue
-                pass
+                continue
 
             #check if suffix matches prefix
-            if(end[-match_len:] == rw[0:match_len]):
-                print("found", rw[::-1])
+            if(end[-match:] == rw[:match]):
+                #print("found", rw[::-1])
                 return False
-            print("rejected", rw[::-1])
-            print(end[-match_len:] + " " + rw[0:match_len])
-            print(len(end[-match_len:]), len(rw[0:match_len]))
-
     #print("found no double links to finish at hyper")
     return True
 
 
 def bfs(d, halves, link_type, start_i, end_i):
+    SINGLE, DOUBLE
 
-    if end_has_no_links(d, end_i, link_type):
+    end = d[end_i]
+
+    if link_type == SINGLE and end_has_no_links(d, end, single_calc, 1):
+        return [0]
+    if link_type == DOUBLE and end_has_no_links(d, end, double_calc, halves[end_i]):
         return [0]
 
     visited = np.array([False] * len(d), dtype=bool)
@@ -164,11 +157,11 @@ def bfs(d, halves, link_type, start_i, end_i):
         l = q.get()
 
         if link_type == SINGLE:
-            n = find_neighbours(d, visited, distance, halves, a_map, l, single_calc, 0)
+            neighbours = find_neighbours_single(d, visited, distance, halves, a_map, l)
         else:
-            n = find_neighbours(d, visited, distance, halves, a_map, l, double_calc, halves[l])
+            neighbours = find_neighbours_double(d, visited, distance, halves, a_map, l)
         
-        for i in n:
+        for i in neighbours:
             if(i == end_i):
                 r = i
                 #create a list of words joining start and end to return
@@ -193,10 +186,13 @@ def single_calc(l, r):
 @memoize
 def double_calc(l, r):
     return max(l, r)
+    
+@memoize
+def start_index(item) :
+    return bisect(dictionary, item)
 
 
-def find_neighbours(dictionary, visited, distance, halves, a_map, l, match_method, start):
-
+def find_neighbours_single(dictionary, visited, distance, halves, a_map, l):
     lw = dictionary[l]
     #minimum length of the matching part of the left word
     suffix_len = halves[l]
@@ -204,28 +200,75 @@ def find_neighbours(dictionary, visited, distance, halves, a_map, l, match_metho
     neighbours = np.array([-1] * len(dictionary), dtype=int)
     count = 0
 
-    for match_len in range(start, len(lw) + 1):
 
-        left = bisect(dictionary, lw[-match_len:])
-        right = bisect(dictionary, lw[-match_len-1:] + chr(ord(lw[-1])+1))
-
+    for match_len in range(1, len(lw) + 1):
+        #print("matching strings of length", match_len)
+        left = start_index(lw[-match_len:])
+        right = start_index(lw[-match_len:-1] + chr(ord(lw[-1])+1))
+        #print(left, right)
         for r, rw in enumerate(dictionary[left:right]):
             r += left
             
             if(visited[r]):
                 continue
+
+            prefix_len = halves[r]
+
+            match = single_calc(suffix_len, prefix_len)
+
+            if(match_len < match):
+                #continue
+                pass
+
+            #check if suffix matches prefix
+            if(lw[-match_len:] == rw[0:match_len]):
+                #print("queued '" + rw + "'")
+                #visit node
+                a_map[r] = l
+                distance[r] = distance[l] + 1
+                #save the neighbour to the return list
+                neighbours[count] = r
+                count += 1
+            #print("rejected '" + rw + "'")
+
+        
+    return neighbours[:count] if neighbours[0] != -1 else []
+
+
+def find_neighbours_double(dictionary, visited, distance, halves, a_map, l):
+    lw = dictionary[l]
+    #minimum length of the matching part of the left word
+    suffix_len = halves[l]
+    #left_len = len(lw)
+
+    neighbours = np.zeros(len(dictionary), dtype=int)
+    count = 0
+
+    #check how big the matching string must be
+    #min_len = double_calc(suffix_len, len(lw))
+    #print(lw)
+    for match_len in range(suffix_len, len(lw) + 1):
+        #print("matching strings of length", match_len)
+        left = start_index(lw[-match_len:])
+        right = start_index(lw[-match_len:-1] + chr(ord(lw[-1])+1))
+        #print("matching a suffix of", lw[-match_len:])
+        for r, rw in enumerate(dictionary[left:right]):
+            r += left   
+
+            if(visited[r]):
+                continue
             
-            if len(rw) > 2 * match_len:
+            if(len(rw) < suffix_len):
                 continue
 
             prefix_len = halves[r]
 
-            match = match_method(suffix_len, prefix_len)
+            match = double_calc(suffix_len, prefix_len)
 
             if(match_len < match):
-                match_len = match
+                continue
 
-            #check if suffix matches prefix
+            #check if suffix matches prefix of next word
             if(lw[-match_len:] == rw[0:match_len]):
                 #visit node
                 a_map[r] = l
@@ -233,10 +276,8 @@ def find_neighbours(dictionary, visited, distance, halves, a_map, l, match_metho
                 #save the neighbour to the return list
                 neighbours[count] = r
                 count += 1
-
-    
         
-    return neighbours[:count] # if neighbours[0] != -1 else []
+    return neighbours[:count]# if neighbours[0] != -1 else []
 
 
 if __name__ == "__main__":
